023d91e810cca43a1f4225a33d9c3ccbdd5ae84e
JiaCheng Zhang <jiachengmadrid@gmail.com>
2024-03-20 20:13:10 +0100

    [Update & Add] LoginScreen name changed to WelcomeScreen and Fonts added

---
+++ 04_Frontend/WewizaFront/app/src/main/java/com/dam/wewiza_front/screens/HomeScreen.kt
@@ -0,0 +1,48 @@
+package com.dam.wewiza_front.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dam.wewiza_front.R
+
+
+@Composable
+fun HomeScreen(
+
+) {
+
+}
+
+@Composable
+fun TopBar() {
+    Box(modifier = Modifier.run {
+        fillMaxWidth()
+        height(100.dp)
+        background(Color.LightGray)
+    }) {
+        Row {
+            Box(modifier = Modifier.size(20.dp)){
+                Image(painter = painterResource(id = R.drawable.provisional_logo), contentDescription ="Logo",  )
+            }
+            Text(text = "Bienvenido a Wewiza!!")
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HomeScreenPreview() {
+    TopBar()
+}
+
---
+++ 04_Frontend/WewizaFront/app/src/main/java/com/dam/wewiza_front/viewModels/WelcomeScreenViewModel.kt
@@ -0,0 +1,36 @@
+package com.dam.wewiza_front.viewModels
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.Firebase
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class WelcomeScreenViewModel: ViewModel() {
+    private val auth: FirebaseAuth = Firebase.auth
+    private val _loading = MutableLiveData(false)
+
+    fun signInWithGoogleCredential(credential: AuthCredential){
+        viewModelScope.launch {
+            try {
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful){
+                            Log.i("Login", "Usuario logueado con google")
+                        }
+                    }
+                    .addOnFailureListener{
+                        Log.i("Login", "error al loguear con google")
+                    }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+}
