2a37bde4e39adfee143bd07d9628bd1ae5d2eb1e
JiaCheng Zhang <jiachengmadrid@gmail.com>
2024-03-26 20:10:56 +0100

    [Add] Bottom nav menu

---
+++ 04_Frontend/WewizaFront/app/src/main/java/com/dam/wewiza_front/ui/theme/MyLightTheme.kt
@@ -0,0 +1,32 @@
+package com.dam.wewiza_front.ui.theme
+
+
+import androidx.compose.foundation.background
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun MyLightTheme(
+    content: @Composable () -> Unit
+){
+
+    fun colors(): ColorScheme {
+        return lightColorScheme(
+            surface = Color(0xFFD0C2DC),
+            onSurface = Color(0xFF2E2C31),
+            primary = Color(0xFFBB84E8)
+        )
+    }
+
+
+    MaterialTheme(
+        content = content,
+        colorScheme = colors()
+    )
+
+
+
+}
