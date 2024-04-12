dcacbb6f122b57b3264fdf7f1b98cea6ac1e8454
JiaCheng Zhang <jiachengmadrid@gmail.com>
2024-04-12 18:00:26 +0200

    [Add] RF-14 Customer support screen completed.

--- 04_Frontend/WewizaFront/app/src/main/java/com/dam/wewiza_front/viewModels/CustomerSupportScreenViewModel.kt
+++ 04_Frontend/WewizaFront/app/src/main/java/com/dam/wewiza_front/viewModels/CustomerSupportScreenViewModel.kt
@@ -1,6 +1,26 @@
 package com.dam.wewiza_front.viewModels
 
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModel
 
 class CustomerSupportScreenViewModel(): ViewModel() {
+    fun sendMessage(userMsg: String, context: Context) {
+        if (userMsg.isBlank()) {
+            Toast.makeText(context, "El mensaje no puede estar vacío", Toast.LENGTH_SHORT).show()
+        } else {
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("jiachengmadrid@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "Mensaje de la aplicación")
+                putExtra(Intent.EXTRA_TEXT, userMsg)
+            }
+
+            if (intent.resolveActivity(context.packageManager) != null) {
+                startActivity(context, Intent.createChooser(intent, "Enviar correo..."), null)
+            }
+        }
+    }
 }
