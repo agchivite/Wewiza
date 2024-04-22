6027309a61cbae295293290417746a5234f53dbf
Angel M.Chivite <sbytmacke@gmail.com>
2024-04-22 20:42:50 +0200

    [Update] Add icons to categories

--- 03_Backend/wewiza/api_wewiza/main.py
+++ 03_Backend/wewiza/api_wewiza/main.py
@@ -29,34 +29,116 @@ def get_categories():
     """
     return {
         "categories": [
-            {"id": "aceite_especias_y_salsas", "name": "Aceite, especias y salsas"},
-            {"id": "agua_y_refrescos", "name": "Agua y refrescos"},
-            {"id": "aperitivos", "name": "Aperitivos"},
-            {"id": "arroz_legumbres_y_pasta", "name": "Arroz, legumbres y pasta"},
+            {
+                "id": "aceite_especias_y_salsas",
+                "name": "Aceite, especias y salsas",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(10).svg?token=A3BM3IAC3RMARDHYZAZAZULGE2XKQ",
+            },
+            {
+                "id": "agua_y_refrescos",
+                "name": "Agua y refrescos",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(11).svg?token=A3BM3ID6NH3TEBASYRUFFZDGE2XNW",
+            },
+            {
+                "id": "aperitivos",
+                "name": "Aperitivos",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(12).svg?token=A3BM3IEKVV2R3ADHTQXK4S3GE2XPK",
+            },
+            {
+                "id": "arroz_legumbres_y_pasta",
+                "name": "Arroz, legumbres y pasta",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(13).svg?token=A3BM3IBAYYL6HPOOCVFDTBTGE2XUE",
+            },
             {
                 "id": "azucar_caramelos_y_chocolate",
                 "name": "Azúcar, caramelos y chocolate",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(14).svg?token=A3BM3IA7FUEKHSTFUA6RMY3GE2XUU",
             },
-            {"id": "bebe", "name": "Bebé"},
-            {"id": "bodega", "name": "Bodega"},
-            {"id": "cacao_cafe_e_infusiones", "name": "Cacao, café e infusiones"},
-            {"id": "carne", "name": "Carne"},
-            {"id": "cereales_y_galletas", "name": "Cereales y galletas"},
-            {"id": "charcuteria_y_quesos", "name": "Charcutería y quesos"},
-            {"id": "congelados", "name": "Congelados"},
-            {"id": "conservas_caldos_y_cremas", "name": "Conservas, caldos y cremas"},
-            {"id": "frutas", "name": "Frutas"},
+            {
+                "id": "bebe",
+                "name": "Bebé",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(15).svg?token=A3BM3IAXDJWZZRQ6Y3T5QADGE2XVC",
+            },
+            {
+                "id": "bodega",
+                "name": "Bodega",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(16).svg?token=A3BM3IBC7MV25MERO6GOFXDGE2XVW",
+            },
+            {
+                "id": "cacao_cafe_e_infusiones",
+                "name": "Cacao, café e infusiones",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(17).svg?token=A3BM3IE3PPUAZDDP3BH6W7TGE2XWE",
+            },
+            {
+                "id": "carne",
+                "name": "Carne",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(18).svg?token=A3BM3IHNHY3NFBACD7HFYJDGE2XWU",
+            },
+            {
+                "id": "cereales_y_galletas",
+                "name": "Cereales y galletas",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(19).svg?token=A3BM3IFVXXSPG3HG4M7JST3GE2XXC",
+            },
+            {
+                "id": "charcuteria_y_quesos",
+                "name": "Charcutería y quesos",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(2).svg?token=A3BM3IBVQC7Z5BRKHCSNIILGE2XXQ",
+            },
+            {
+                "id": "congelados",
+                "name": "Congelados",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(20).svg?token=A3BM3IAURP55NUZ6TRCMWJTGE2XYA",
+            },
+            {
+                "id": "conservas_caldos_y_cremas",
+                "name": "Conservas, caldos y cremas",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(21).svg?token=A3BM3IERT67UMPV6ZFZSC3LGE2XYO",
+            },
+            {
+                "id": "frutas",
+                "name": "Frutas",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(3).svg?token=A3BM3IDU4TBGEQ34GWFDQW3GE2XZC",
+            },
             {
                 "id": "huevos_leche_batidos_y_mantequilla",
                 "name": "Huevos, leche, batidos y mantequilla",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(4).svg?token=A3BM3IEFMRIQBURVLXACT5TGE2XZS",
             },
-            {"id": "marisco_y_pescado", "name": "Marisco y pescado"},
-            {"id": "mascotas", "name": "Mascotas"},
-            {"id": "panaderia_y_pasteleria", "name": "Panadería y pastelería"},
-            {"id": "pizzas_y_platos_preparados", "name": "Pizzas y platos preparados"},
-            {"id": "postres_y_yogures", "name": "Postres y yogures"},
-            {"id": "verduras", "name": "Verduras"},
-            {"id": "zumos", "name": "Zumos"},
+            {
+                "id": "marisco_y_pescado",
+                "name": "Marisco y pescado",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(5).svg?token=A3BM3IE2PJXW6VX46IFGST3GE2XZ6",
+            },
+            {
+                "id": "mascotas",
+                "name": "Mascotas",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(6).svg?token=A3BM3IAESKVJJBMDSR6Q56LGE2X2M",
+            },
+            {
+                "id": "panaderia_y_pasteleria",
+                "name": "Panadería y pastelería",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(7).svg?token=A3BM3ICOOVUBRZXWE2VEU5TGE2X24",
+            },
+            {
+                "id": "pizzas_y_platos_preparados",
+                "name": "Pizzas y platos preparados",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(8).svg?token=A3BM3ICQ6X4X7CWQD33555LGE2X3K",
+            },
+            {
+                "id": "postres_y_yogures",
+                "name": "Postres y yogures",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia%20(9).svg?token=A3BM3IHGCSDTE3UJCWKX7QLGE2X32",
+            },
+            {
+                "id": "verduras",
+                "name": "Verduras",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com%20-%20copia.svg?token=A3BM3IH3TPO5AXDVHNBEES3GE2X4G",
+            },
+            {
+                "id": "zumos",
+                "name": "Zumos",
+                "icon": "https://raw.githubusercontent.com/JiaChengZhang14/Wewiza-Icons/a861f399f2f4a6c9f3bb6f79fe88b2fc3fd848ad/category-svgrepo-com.svg?token=A3BM3ICA3SEATE6FV24NWODGE2X4U",
+            },
         ]
     }
 
