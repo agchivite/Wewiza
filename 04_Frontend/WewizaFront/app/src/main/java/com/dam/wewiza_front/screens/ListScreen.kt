f2c905d1967783064dba2d26e6d8970ce305c9a3
JiaCheng Zhang <jiachengmadrid@gmail.com>
2024-05-15 14:05:24 +0200

    [Add] User's ShoppingList total price and suggestion buttom

--- 04_Frontend/WewizaFront/app/src/main/java/com/dam/wewiza_front/screens/ListScreen.kt
+++ 04_Frontend/WewizaFront/app/src/main/java/com/dam/wewiza_front/screens/ListScreen.kt
@@ -1,11 +1,14 @@
 package com.dam.wewiza_front.screens
 
+
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,9 +16,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -33,7 +36,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.rememberImagePainter
 import com.dam.wewiza_front.constants.Constants
@@ -48,7 +53,7 @@ fun ListScreen(listScreenViewModel: ListScreenViewModel, navController: NavHostC
     val selectedProductsIds = sharedViewModel.selectedList.value
     val productsList = remember { mutableStateOf(emptyList<Product>()) }
     Log.d("ListScreen", "Selected Products: ${selectedProductsIds!!.products.size}")
-    var loading by  remember { mutableStateOf(false)}
+    var loading by remember { mutableStateOf(false) }
 
     if (selectedProductsIds!!.products.size > 0) {
 
@@ -86,26 +91,58 @@ fun ListScreenBodyContent(
     productsList: MutableState<List<Product>>
 ) {
     val productsList = productsList.value
-
     Column(
         modifier = Modifier
-            .padding(top = 70.dp)
             .fillMaxSize()
+            .padding(top = 70.dp)
             .background(Color.LightGray)
     ) {
-        LazyColumn {
+        // Columna con la lista de productos
+        LazyColumn(
+            modifier = Modifier.weight(1f) // Ocupa todo el espacio restante
+        ) {
             items(productsList.size) { index ->
                 ListProductItem(product = productsList[index])
             }
         }
-    }
 
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .background(Color.Gray),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                //product list total price
+                Text(text = "Total: ${"%.2f".format(productsList.sumOf { it.price })} €", style = TextStyle(fontSize = 20.sp))
+
+
+                Button(onClick = { /* Aquí puedes colocar la lógica para sugerir */ }) {
+                    Text(text = "Sugerir")
+                }
+            }
+        }
+    }
 }
 
 
 @Composable
 fun ListProductItem(product: Product) {
-    Card(shape = RoundedCornerShape(10.dp), modifier = Modifier.fillMaxWidth().height(110.dp).padding(8.dp)) {
+    Card(
+        shape = RoundedCornerShape(10.dp), modifier = Modifier
+            .fillMaxWidth()
+            .height(110.dp)
+            .padding(8.dp)
+    ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
                 painter = rememberImagePainter(data = product.image_url),
